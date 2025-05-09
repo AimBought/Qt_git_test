c2ccf89e2a8a8508c694c389b2b0a197a4872b12
jakub stelmach <jakub.stelmach05@gmail.com>
2025-05-09 19:57:40 +0200

    guwno

---
+++ build/Desktop_Qt_6_9_0_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Qt_repo_FILE C:/Users/jakub/Documents/QtTest/Qt_git_test/build/Desktop_Qt_6_9_0_MSVC2022_64bit-Debug/Qt_repo.exe)
+set(__QT_DEPLOY_TARGET_Qt_repo_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Qt_repo_RUNTIME_DLLS C:/Qt/6.9.0/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Cored.dll)
